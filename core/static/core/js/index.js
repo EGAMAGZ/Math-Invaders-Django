2406a63823a93843ae02ff12854c47a1eba9044d
EGAMAGZ <gazapito1@hotmail.com>
2020-02-20 21:22:41 -0600

    Added requirements.txt

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -1,25 +1,4 @@
 $("#play").on("click", function() {
-  var menu = false;
-  if ($("#play").text() === "Jugar!") {
-    $(".form-jugar").css({ bottom: "0em" });
-    $("#play").text("Cerrar");
-
-    $(".form-register").css({ top: "-30em" });
-  } else {
-    $(".form-jugar").css({ bottom: "-35em" });
-    $("#play").text("Jugar!");
-    $(".form-register").css({ top: "-30em" });
-  }
-});
-
-$("#register").on("click", function() {
-  var menu2 = false;
-  if (menu2) {
-    $(".form-register").css({ top: "-30px" });
-  } else {
-    $(".form-register").css({ top: "0px" });
-    $(".form-jugar").css({ bottom: "-35em" });
-  }
 });
 
 $(document).on("ready", function() {
