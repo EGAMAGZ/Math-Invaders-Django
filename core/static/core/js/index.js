b18165801e47894ba958865cd479ac1172cb6ca4
EGAMAGZ <gazapito1@hotmail.com>
2020-02-20 23:00:23 -0600

    Changed few things such as subtitle of advice and commented the js code section that detected key events

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -1,12 +1,11 @@
-alert("Hola");
-$(document).on("ready", function() {
-  $(this).keydown(function(event) {
-    var anuncio_display = $(".anuncio-container").css("display");
-    if (anuncio_display === "flex") {
-      $(".anuncio-container").css({ display: "none" });
-    }
-  });
-});
+// $(document).on("ready", function() {
+//   $(this).keydown(function(event) {
+//     var anuncio_display = $(".anuncio-container").css("display");
+//     if (anuncio_display === "flex") {
+//       $(".anuncio-container").css({ display: "none" });
+//     }
+//   });
+// });
 $("#close-advice").on("click", function() {
   $(".anuncio-container").css({ display: "none" });
 });
