e380145a50f2da0a68518cd2608a533358b19129
EGAMAGZ <gazapito1@hotmail.com>
2020-02-26 19:09:30 -0600

    Sucessful try to change jquery to vanilla js

--- core/static/core/js/index.js
+++ core/static/core/js/index.js
@@ -1,9 +1,1 @@
-
-var close_advice=document.getElementById("close-advice").onclick=()=>document.getElementById("advice-container").style.display=none;
-// $("#close-advice").on("click", function() {
-//   $(".advice-container").css({ display: "none" });
-// });
-
-// function display_alert() {
-//   $(".advice-container").css({ display: "flex" });
-// }
+var close_advice=document.getElementById("close-advice").onclick=()=>document.getElementById("advice-container").style.display="none";
